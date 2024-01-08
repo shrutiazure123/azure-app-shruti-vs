42b47e20c42794ee89e10c6cdde3e573ba4c3d95
shrutiazure123 <shrutiazure123@gmail.com>
2024-01-08 14:02:39 +0530

    added configuration

--- azure-app-shruti-vs/Pages/Index.cshtml.cs
+++ azure-app-shruti-vs/Pages/Index.cshtml.cs
@@ -6,15 +6,16 @@ namespace azure_app_shruti_vs.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly IConfiguration _configuration;
+        public IndexModel(ILogger<IndexModel> logger,IConfiguration configuration)
         {
             _logger = logger;
+            this._configuration = configuration;
         }
 
         public void OnGet()
         {
-
+            ViewData["Greetings"] = _configuration["Greetings"];
         }
     }
 }
--- azure-app-shruti-vs/Pages/Privacy.cshtml.cs
+++ azure-app-shruti-vs/Pages/Privacy.cshtml.cs
@@ -14,6 +14,7 @@ namespace azure_app_shruti_vs.Pages
 
         public void OnGet()
         {
+            throw new Exception("testing application insights");
         }
     }
 }
